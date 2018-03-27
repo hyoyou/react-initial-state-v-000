3aee4d7cdbf02ddb6964938c9a269bb05f9a2214
Heather You <heatherhyou@gmail.com>
2018-03-27 11:50:48 -0400

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,31 @@
-// Component Code Goes Here
+import React from 'react';
+
+/* Storing computed values in your state like below should be avoided
+class Address extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullAddress: `${props.street}, ${props.city}`
+    }
+  }
+
+  render() {
+    return (
+      <div className="address">
+        {this.state.fullAddress}
+      </div>
+    );
+  }
+}
+*/
+
+class Address extends React.Component {
+  render() {
+    return (
+      <div className="address">{this.props.street}, {this.props.city}</div>
+    );
+  }
+}
+
+export default Address;
